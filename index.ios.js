bf06337c8944191890ca759910ddb1d3da1ebd86
Harry Wolff <hswolff@gmail.com>
2017-01-16 17:47:00 -0500

    Completed Exercise 4

--- index.ios.js
+++ index.ios.js
@@ -6,11 +6,18 @@
 
 import React, { Component } from 'react';
 import {
+  ART,
   AppRegistry,
   StyleSheet,
   Text,
   View
 } from 'react-native';
+
+const {
+  Group,
+  Shape,
+  Surface,
+} = ART;
 
 export default class ReactNativeAndD3Workshop extends Component {
   render() {
@@ -22,6 +29,15 @@ export default class ReactNativeAndD3Workshop extends Component {
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
         </Text>
+        <Surface width={200} height={200}>
+          <Group x={0} y={0}>
+            <Shape
+              d="M10 80 C 40 10, 65 10, 95 80 S 150 150, 180 80"
+              stroke="#000"
+              strokeWidth={1}
+            />
+          </Group>
+        </Surface>
         <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
